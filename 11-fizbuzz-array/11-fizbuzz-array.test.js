e2f4bc96443631018147f7f38ebbcb40415b9497
milanr21 <67459437+milanr21@users.noreply.github.com>
2026-09-19 17:22:09 +0545

    fizzbuzz

---
+++ 11-fizbuzz-array/11-fizbuzz-array.test.js
@@ -0,0 +1,21 @@
+const fizzBuzzArray = require("./11-fizbuzz-array");
+
+test("FizzBuzz Array", () => {
+  expect(fizzBuzzArray(15)).toEqual([
+    1,
+    2,
+    "Fizz",
+    4,
+    "Buzz",
+    "Fizz",
+    7,
+    8,
+    "Fizz",
+    "Buzz",
+    11,
+    "Fizz",
+    13,
+    14,
+    "FizzBuzz",
+  ]);
+});
